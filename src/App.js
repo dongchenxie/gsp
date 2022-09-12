db3deff9ee3bc325ffb8dd8cfbba94dd8c231f40
Vibha Rana <ranavibha02@gmail.com>
2022-09-11 23:59:42 -0700

    API called for demo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from "react";
+
+import useFetchJobs from './useFetchJobs'
+import {Container} from 'react-bootstrap'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   const {jobs, loading, error} = useFetchJobs()
+  const JOBS_API = 'https://projects.test.api.goldspot.ca'
+  
+   return (
+    <Container>
+      {loading && <h1>Loading...</h1>}
+      {error && <h1>Error. Try Again Later</h1>}
+      <h1>{jobs.length}</h1>
+      
+    </Container>
   );
 }
 
 export default App;
+
